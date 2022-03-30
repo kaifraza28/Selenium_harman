149bbae4e87c5e8c9cfa2a06c2d5685e394ba4eb
kaifraza28 <itzkaifraza@gmail.com>
2022-03-30 17:21:34 +0530

    Instagramk Login

--- test_instagramlogin.py
+++ test_instagramlogin.py
@@ -2,6 +2,8 @@ import time
 
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+user_input = input("Please Enter your user_name\n")
+user_input2 = input("Please Enter your Password\n")
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
@@ -9,10 +11,11 @@ print("Test case started!")
 driver.maximize_window()
 driver.get("https://www.instagram.com/accounts/login/")
 time.sleep(1)
-driver.find_element_by_name("username").send_keys("kaif.__.raza")
-driver.find_element_by_name("password").send_keys("K@ppu2801")
-driver.find_element_by_class_name("EPjEi").click()
-time.sleep(1)
+driver.find_element_by_name("username").send_keys(user_input)
+driver.find_element_by_name("password").send_keys(user_input2)
+driver.find_element_by_xpath("//button[@type='submit']").click()
+# driver.find_element_by_class_name("_9zn7").click()
+time.sleep(10)
 driver.close()
 print("Instagram login done succesufully")
 print("Test case has sucessfully completed!")
