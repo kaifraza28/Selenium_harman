42e2f743876349c1594ccc23ff1150fc24f7cb81
kaifraza28 <itzkaifraza@gmail.com>
2022-03-30 15:26:58 +0530

    Selenium Testing

--- test_googlesearch.py
+++ test_googlesearch.py
@@ -6,6 +6,12 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 print("Test Case Started")
 driver.maximize_window()
 driver.get("https://www.google.com/")
+time.sleep(1)
+driver.find_element_by_name("q").send_keys("Harman")
+time.sleep(2)
+driver.find_element_by_name("btnK").click()
+
+
 time.sleep(4)
 driver.close()
 print("Test cases has successfully completed")
